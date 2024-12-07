ed19e305972b01c182232df513926e592c0e48fb
mouad kaaib <elcosto02@gmail.com>
2024-12-07 17:23:39 +0100

    add login view | add login controller | add images of login page | add css files

--- src/main/java/com/example/buildingeye/controllers/HelloController.java
+++ src/main/java/com/example/buildingeye/controllers/HelloController.java
@@ -1,14 +1,49 @@
 package com.example.buildingeye.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class HelloController {
+
+    @FXML
+    private Button facialRecognitorButton;
+
+    @FXML
+    private Button loginButton;
+
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void openFacialRecognitor(ActionEvent event) {
+
     }
+
+    @FXML
+    void openLoginStage(ActionEvent event) {
+        URL fxmlLocation = getClass().getResource("/com/example/buildingeye/views/login-view.fxml");
+        if (fxmlLocation == null) {
+            throw new IllegalStateException("FXML file not found!");
+        }
+        FXMLLoader loader = new FXMLLoader(fxmlLocation);
+        try {
+            Parent root = loader.load();
+            Scene newScene = new Scene(root);
+            Stage newWindow = new Stage();
+            newWindow.setScene(newScene);
+            newWindow.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
---
+++ src/main/java/com/example/buildingeye/controllers/LoginController.java
@@ -0,0 +1,32 @@
+package com.example.buildingeye.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    void onClearClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLoginClick(ActionEvent event) {
+
+    }
+
+}
