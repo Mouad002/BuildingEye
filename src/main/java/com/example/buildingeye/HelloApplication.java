dcb82de96d3f3469c1fc29f6c795a19e78d3cb1c
lilyhabti <105781552+lilyhabti@users.noreply.github.com>
2024-12-21 16:51:47 +0100

    Fixed FXML loading and removed unused image from dashboard view

--- src/main/java/com/example/buildingeye/HelloApplication.java
+++ src/main/java/com/example/buildingeye/HelloApplication.java
@@ -1,24 +1,17 @@
 package com.example.buildingeye;
 
-import com.example.buildingeye.functional.SingletonConnection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),800,600);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/buildingeye/views/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("BuildingEye");
         stage.setScene(scene);
         stage.show();
     }
